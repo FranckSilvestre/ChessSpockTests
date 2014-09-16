bf5315be1239d3dff2d30fd7ebbdc718cb302591
Franck Silvestre <franck.silvestre@ticetime.com>
2014-09-16 15:05:47 +0200

    Amélioration tests

--- src/test/groovy/chess/PionSpecification3.groovy
+++ src/test/groovy/chess/PionSpecification3.groovy
@@ -13,31 +13,53 @@ import static chess.PieceCouleur.NOIR
 class PionSpecification3 extends Specification {
 
   @Unroll
-  def "deplacement du pion"() {
+  def "deplacement du pion correcte"() {
 
-    setup:
+    given: "un pion dans une position donnée"
     def pion = new Pion(positionHorizontale: posHor, positionVerticale: posVer, pieceCouleur: couleur)
 
-    when:
+    when: "un déplacement correct du pion est exécutée"
     def depOk = pion.bouge(depHor, depVer)
 
-    then:
-    depOk == depCorrect
-    if (depOk) {
-      pion.positionHorizontale == ((int)posHor)+depHor
-      pion.positionVerticale == posVer+depVer
-    }
+    then: "le déplacement est correctement effectué"
+    depOk == true
+
+    and:"la position du pion est bien mise à jour"
+    pion.positionHorizontale == ((int)posHor)+depHor
+    pion.positionVerticale == posVer+depVer
+
 
     where:
-    couleur | posHor | posVer | depHor | depVer | depCorrect
-    BLANC   | "a"    | 2      | 0      | 3      | false
-    BLANC   | "a"    | 2      | 0      | 2      | true
-    BLANC   | "a"    | 3      | 0      | 2      | false
-    BLANC   | "a"    | 3      | 0      | 1      | true
-    BLANC   | "a"    | 3      | 0      | -1     | false
-    NOIR    | "a"    | 7      | 0      | -1     | true
-    NOIR    | "a"    | 7      | 0      | 1      | false
+    couleur | posHor | posVer | depHor | depVer
+    BLANC   | "a"    | 2      | 0      | 2
+    BLANC   | "a"    | 3      | 0      | 1
+    NOIR    | "a"    | 7      | 0      | -1
 
   }
+
+    @Unroll
+    def "deplacement du pion incorrecte"() {
+
+        given: "un pion dans une position donnée"
+        def pion = new Pion(positionHorizontale: posHor, positionVerticale: posVer, pieceCouleur: couleur)
+
+        when: "un déplacement incorrect du pion est exécuté"
+        def depOk = pion.bouge(depHor, depVer)
+
+        then: "le déplacement est indiqué comme non effectué"
+        depOk == false
+
+        and:"le pion est resté dans sa position initiale"
+        pion.positionHorizontale == (int)posHor
+        pion.positionVerticale == posVer
+
+        where:
+        couleur | posHor | posVer | depHor | depVer
+        BLANC   | "a"    | 2      | 0      | 3
+        BLANC   | "a"    | 3      | 0      | 2
+        BLANC   | "a"    | 3      | 0      | -1
+        NOIR    | "a"    | 7      | 0      | 1
+
+    }
 
 }
